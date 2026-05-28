572d0b0590ae6a9b3224bac342b66b6d1845387e
Cecilia Gómez <cecidycv@gmail.com>
2026-05-28 18:08:19 -0300

    Ajuste JS para mostrar MP4

--- galeria.js
+++ galeria.js
@@ -1,10 +1,48 @@
 const contenedor = document.getElementById("galeria");
-
 const imagenes = JSON.parse(contenedor.dataset.imagenes);
 
 let indice = 0;
+let imagen = document.getElementById("imagen-galeria");
+let video = null;
 
-const imagen = document.getElementById("imagen-galeria");
+function esVideo(url) {
+    return url.endsWith(".mp4") || url.endsWith(".webm") || url.endsWith(".ogg");
+}
+
+function mostrarMedia() {
+    const url = imagenes[indice];
+
+    if (esVideo(url)) {
+
+        if (!video) {
+            video = document.createElement("video");
+            video.id = "imagen-galeria";
+            video.controls = true;
+            video.autoplay = true;
+            video.loop = true;
+            video.style.maxWidth = "100%";
+
+            imagen.replaceWith(video);
+        }
+
+        video.src = url;
+        video.play();
+
+    } else {
+
+        if (video) {
+            const img = document.createElement("img");
+            img.id = "imagen-galeria";
+
+            video.replaceWith(img);
+
+            imagen = img;
+            video = null;
+        }
+
+        imagen.src = url;
+    }
+}
 
 function siguiente() {
     indice++;
@@ -13,7 +51,7 @@ function siguiente() {
         indice = 0;
     }
 
-    imagen.src = imagenes[indice];
+    mostrarMedia();
 }
 
 function anterior() {
@@ -23,5 +61,5 @@ function anterior() {
         indice = imagenes.length - 1;
     }
 
-    imagen.src = imagenes[indice];
+    mostrarMedia();
 }
